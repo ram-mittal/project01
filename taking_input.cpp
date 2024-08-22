d95b28b1de0f0ff519ca4b2d7b25561fc2246dd0
ram-mittal <rammittal0111@gmail.com>
2024-08-22 22:16:53 +0530

    Create taking_input.cpp

---
+++ taking_input.cpp
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+
+ int main(){
+    int x;
+    printf ("enter first number : ");
+    scanf("%d", &x);
+    int y;
+    printf ("enter second number : ");
+    scanf("%d",&y);
+    int z = x + y;
+    printf ("sum of the numbers is :");
+    printf("%d",z);
+}
